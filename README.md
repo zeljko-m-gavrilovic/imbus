b048f3bd7e0ccc6d2d508cd4b391c3cc77e76f43
zeljko.gavrilovic <zeljko.m.gavrilovic@gmail.com>
2016-02-23 17:22:11 +0100

    creating windy package

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-ECHO is on.
+Windy is an application for managing Windows user/system environment variables. 
+
+Features:
+- Add/update/remove of the environment variables
+- Batch import of the environment variables from a file or a clipboard
+- Resizable GUI with the examined views
+- Absolutely free and open source 
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='windy',
+      version='0.8',
+      description='Windy is an application for managing Windows user/system environment variables.',
+      url='http://github.com/zeljko.m.gavrilovic/windy',
+      author='BigNumbers',
+      author_email='zeljko.m.gavrilovic@gmail.com',
+      license='MIT',
+      packages=['windy'],
+      zip_safe=False)
